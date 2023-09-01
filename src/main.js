22d9b7206c9fa90e7c93529f276d16a1e6512214
Adrien Michaud <adrien.michaud@epitech.eu>
2023-09-01 19:26:52 +0200

    create grid and make pacman moveable

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createStore } from 'vuex';
+import pacmanStore from '../store/pacman/index.js';
 
-createApp(App).mount('#app')
+const store = createStore(pacmanStore);
+
+createApp(App).use(store).mount('#app');
